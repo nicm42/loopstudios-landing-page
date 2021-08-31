0f24e9ffefb953a3723e22534573d3a473757f84
Nic <42087659+nicm42@users.noreply.github.com>
2021-08-31 17:17:02 +0100

    Added scripts and styles to get menu to show and hide

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log('Hello world');
+const menu = document.querySelector('.header__menu') as HTMLButtonElement;
+const nav = document.querySelector('.header__nav') as HTMLUListElement;
+const buttonImage = document.querySelector(
+  '.header__hamburger'
+) as HTMLImageElement;
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('open');
+  if (menu.classList.contains('open')) {
+    buttonImage.src = 'public/icon-close.svg';
+  } else {
+    buttonImage.src = 'public/icon-hamburger.svg';
+  }
+});
