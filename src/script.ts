5b22b923514ef55883595c3437448fee49aaf75b
Nic <42087659+nicm42@users.noreply.github.com>
2021-09-03 14:11:30 +0100

    Updated nav menu to close when click on links

--- src/script.ts
+++ src/script.ts
@@ -3,6 +3,7 @@ const nav = document.querySelector('.header__nav') as HTMLUListElement;
 const buttonImage = document.querySelector(
   '.header__hamburger'
 ) as HTMLImageElement;
+const links = document.querySelectorAll('.header__link') as HTMLLinkElement;
 
 menu.addEventListener('click', () => {
   nav.classList.toggle('open');
@@ -11,4 +12,10 @@ menu.addEventListener('click', () => {
   } else {
     buttonImage.src = 'public/icon-hamburger.svg';
   }
+});
+
+Array.from(links).map((link) => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('open');
+  });
 });
